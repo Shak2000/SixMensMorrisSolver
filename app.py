6b7367de9941deaa56bf33c7650c7ffd6bb996e4
Shak Ragoler <shak.ragoler@gmail.com>
2025-08-01 13:35:15 -0700

    Adding an API

---
+++ app.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Query
+from fastapi.responses import FileResponse
+from main import Game
+
+app = FastAPI()
+game = Game()
+
+
+@app.get("/")
+async def get_ui():
+    return FileResponse("index.html")
+
+
+@app.get("/styles.css")
+async def get_styles():
+    return FileResponse("styles.css")
+
+
+@app.get("/script.js")
+async def get_script():
+    return FileResponse("script.js")
+
+
+@app.post("/start")
+async def start():
+    game.start()
+
+
+@app.post("/switch")
+async def switch():
+    game.switch()
+
+
+@app.post("/place")
+async def place(x: int, y: int):
+    game.place(x, y)
+
+
+@app.post("/move")
+async def move(x: int, y: int, nx: int, ny: int):
+    game.move(x, y, nx, ny)
+
+
+@app.get("/get_piece_count")
+async def get_piece_count(player: str):
+    return game.get_piece_count(player)
+
+
+@app.get("/check_mill")
+async def check_mill(x: int, y: int, player: str):
+    return game.check_mill(x, y, player)
+
+
+@app.get("/get_opponent_pieces")
+async def get_opponent_pieces(player: str):
+    return game.get_opponent_pieces(player)
+
+
+@app.post("/remove_piece")
+async def remove_piece(x: int, y: int, player: str):
+    game.remove_piece(x, y, player)
+
+
+@app.get("/check_win")
+async def check_win():
+    return game.check_win()
+
+
+@app.get("/has_valid_moves")
+async def has_valid_moves(player: str):
+    return game.has_valid_moves(player)
+
+
+@app.post("/undo")
+async def undo():
+    game.undo()
